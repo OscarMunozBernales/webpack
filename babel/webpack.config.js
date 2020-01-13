186e597c1ed79a537e0c61af4e56fd4f2bf51053
Oscar Muñoz Bernales <munoz.bernales.oscar@gmail.com>
2020-01-13 09:19:02 -0300

    Babel - soporte de JS moderno

---
+++ babel/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const MiniCSSExtractPLugin = require('mini-css-extract-plugin')
+const HtmlWebpackPLugins = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: {
+        index :  path.resolve(__dirname, 'src/js/index.js')
+    },
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules : [
+            {
+                test    : /\.js$/,
+                use     : 'babel-loader',
+                exclude : /node_module/,
+            },
+            {
+                test : /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    plugins : [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPLugins({
+            title: 'Plugins'
+        }),
+        // new MiniCSSExtractPLugin({
+        //     filename : 'css/[name].css'
+        // })
+    ],
+    devServer : {
+        hot : true,
+        open : true,
+        port : 9000
+    }
+}
