1cedd3e282ce972a835ac6ce46de2e4ad2a1ae66
Oscar Muñoz Bernales <munoz.bernales.oscar@gmail.com>
2020-01-13 10:10:46 -0300

    Trabajando con JS moderno.

--- babel/src/js/index.js
+++ babel/src/js/index.js
@@ -1,11 +1,23 @@
 import '../css/index.css'
 
-import text from './text'
+// import text from './text'
 
 
-if(module.hot){
-    module.hot.accept('./text.js', () => {
-        console.log("He recargado en caliente!")
-        text()
+// if(module.hot){
+//     module.hot.accept('./text.js', () => {
+//         console.log("He recargado en caliente!")
+//         text()
+//     })
+// }
+
+import search from './search'
+import render from './render'
+const id = prompt('Quien es ese pokemon?')
+
+search(id)
+    .then( (data) => {
+        render(data)
     })
-}
+    .catch( () => {
+        console.log("No hubo pokemon")
+    })
