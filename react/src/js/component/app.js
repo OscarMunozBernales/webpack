ab071da1a3df08b0f145b8351917d27b4d3d0c98
Oscar Muñoz Bernales <munoz.bernales.oscar@gmail.com>
2020-01-14 09:54:59 -0300

    Trabajando con react

--- react/src/js/component/app.js
+++ react/src/js/component/app.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './loader'
 
+console.log(data)
 function App() {
+    const [loaderList, setLoaderList] = useState([])
+    function handleClick() {
+        setLoaderList(data.Loaders)
+    }
     return (
         <div>
+            <ul>
+                {
+                    loaderList.map( item => <Loader data={item} key={item.id}/>)
+                }
+            </ul>
+            <button onClick={handleClick}> Mostrar lo aprendido hasta el momento</button>
             Que linda app hecha en Reacj.js
         </div>
     )
